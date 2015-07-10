ef323c13f989cfdb66c7ce7f8d29ed7b2e80ebe5
Herr Paul <weichwarenerpel@gmail.com>
2015-07-10 23:17:59 +0200

    first interfaces \m/

---
+++ src/de/erpel/metalarchives/Band.java
@@ -0,0 +1,20 @@
+package de.erpel.metalarchives;
+
+public interface Band {
+	String GetName();
+	String GetCountry();
+	String GetLocation();
+	String GetStatus();
+	int GetFormedIn();
+	String GetYearsActive();
+	String GetGenre();
+	String GetLyricalThemes();
+	String GetLastLabel();
+	String GetDescription();
+	//logo
+	//photos
+	//releases
+	//members
+	//similar
+	//links
+}
---
+++ src/de/erpel/metalarchives/Metalarchives.java
@@ -0,0 +1,7 @@
+package de.erpel.metalarchives;
+
+import java.util.Collection;
+
+public interface Metalarchives {
+	Collection<Band> GetBand(String search);
+}
